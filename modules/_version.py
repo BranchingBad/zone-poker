ff9917570e3235eda85291ceaa8e05d86311e302
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2025-11-13 21:35:07 -0500

    pre-commit and linting

--- modules/_version.py
+++ modules/_version.py
@@ -12,8 +12,7 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple
-    from typing import Union
+    from typing import Tuple, Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -28,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.10.dev5+g29a4aa92d.d20251114'
-__version_tuple__ = version_tuple = (1, 0, 10, 'dev5', 'g29a4aa92d.d20251114')
+__version__ = version = "1.0.10.dev5+g29a4aa92d.d20251114"
+__version_tuple__ = version_tuple = (1, 0, 10, "dev5", "g29a4aa92d.d20251114")
 
-__commit_id__ = commit_id = 'g29a4aa92d'
+__commit_id__ = commit_id = "g29a4aa92d"
